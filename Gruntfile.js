215daa7d3a263c8d43f3577975422cc2144f3f54
baden <baden.i.ua@gmail.com>
2014-05-20 14:26:42 +0300

    Base structure.

---
+++ Gruntfile.js
@@ -0,0 +1,150 @@
+module.exports = function(grunt) {
+	"use strict";
+
+	var buildPlatforms = parseBuildPlatforms(grunt.option('platforms'));
+
+	require('load-grunt-tasks')(grunt);
+
+	grunt.registerTask('default', [
+		'stylus'
+	]);
+
+	grunt.registerTask('css', [
+		'stylus'
+	]);
+
+	grunt.registerTask('nodewkbuild', [
+		'nodewebkit:build'
+	]);
+
+	grunt.registerTask('build', [
+		'default',
+		'nodewebkit:build'
+	]);
+
+	grunt.registerTask('dist', [
+		'default',
+		'nodewebkit:dist',
+		'copy:package'
+	]);
+
+	grunt.initConfig({
+		bump: {
+			options: {
+				files: ['package.json'],
+				pushTo: 'origin'
+			}
+		},
+
+		stylus: {
+			compile: {
+				options: {
+					compress: false,
+					'resolve url': true,
+					use: ['nib'],
+					paths: ['src/app/styl']
+				},
+				files: {
+					'src/app/css/app.css' : 'src/app/styl/app.styl'
+				}
+			}
+		},
+
+		nodewebkit: {
+			build: {
+				options: {
+					version: '0.9.2',
+					build_dir: './build', // Where the build version of my node-webkit app is saved
+					mac_icns: './src/app/images/popcorntime.icns', // Path to the Mac icon file
+					mac: buildPlatforms.mac,
+					win: buildPlatforms.win,
+					linux32: buildPlatforms.linux32,
+					linux64: buildPlatforms.linux64,
+					download_url: 'http://nw.get-popcorn.com/'
+				},
+				src: ['./src/**', './node_modules/**', '!./node_modules/bower/**', '!./node_modules/grunt*/**', '!./node_modules/stylus/**', './package.json', './README.md', './LICENSE.txt' ]
+			},
+			dist: {
+				options: {
+					version: '0.9.2',
+					build_dir: './build', // Where the build version of my node-webkit app is saved
+					embed_nw: false, // Don't embed the .nw package in the binary
+					keep_nw: true,
+					mac_icns: './src/app/images/popcorntime.icns', // Path to the Mac icon file
+					mac: buildPlatforms.mac,
+					win: buildPlatforms.win,
+					linux32: buildPlatforms.linux32,
+					linux64: buildPlatforms.linux64,
+					download_url: 'http://nw.get-popcorn.com/'
+				},
+				src: ['./src/**', './node_modules/**', '!./node_modules/bower/**', '!./node_modules/grunt*/**', '!./node_modules/stylus/**', './package.json', './README.md', './LICENSE.txt' ]
+			}
+		},
+
+		copy: {
+			package: {
+				files: [
+					{
+						src: 'build/releases/TTY-Logger/TTY-Logger.nw',
+						dest: 'build/releases/TTY-Logger/linux32/TTY-Logger/package.nw',
+						flatten: true
+					},{
+						src: 'build/releases/TTY-LoggerTTY-Logger.nw',
+						dest: 'build/releases/TTY-Logger/linux64/TTY-Logger/package.nw',
+						flatten: true
+					}
+				]
+			}
+		},
+
+		exec: {
+			win: {
+				cmd: '"build/cache/win/<%= nodewebkit.build.options.version %>/nw.exe" .'
+			},
+			mac: {
+				cmd: 'build/cache/mac/<%= nodewebkit.build.options.version %>/node-webkit.app/Contents/MacOS/node-webkit .'
+			},
+			linux32: {
+				cmd: '"build/cache/linux32/<%= nodewebkit.build.options.version %>/nw" .'
+			},
+			linux64: {
+				cmd: '"build/cache/linux64/<%= nodewebkit.build.options.version %>/nw" .'
+			}
+		},
+
+		watch: {
+			options: {
+				dateFormat: function(time) {
+					grunt.log.writeln('Completed in ' + time + 'ms at ' + (new Date()).toLocaleTimeString());
+					grunt.log.writeln('Waiting for more changes...');
+				},
+			},
+			scripts: {
+				files: ['./src/app/styl/*.styl','./src/app/styl/**/*.styl'],
+				tasks: ['css']
+			},
+		},
+	});
+
+};
+
+var parseBuildPlatforms = function(argumentPlatform) {
+	// this will make it build no platform when the platform option is specified
+	// without a value which makes argumentPlatform into a boolean
+	var inputPlatforms = argumentPlatform || process.platform + ";" + process.arch;
+
+	// Do some scrubbing to make it easier to match in the regexes bellow
+	inputPlatforms = inputPlatforms.replace("darwin", "mac");
+	inputPlatforms = inputPlatforms.replace(/;ia|;x|;arm/, "");
+
+	var buildAll = /^all$/.test(inputPlatforms);
+
+	var buildPlatforms = {
+		mac: /mac/.test(inputPlatforms) || buildAll,
+		win: /win/.test(inputPlatforms) || buildAll,
+		linux32: /linux32/.test(inputPlatforms) || buildAll,
+		linux64: /linux64/.test(inputPlatforms) || buildAll
+	};
+
+	return buildPlatforms;
+}
---
+++ src/app/app.js
@@ -0,0 +1,110 @@
+var
+    // Load native UI library
+    gui = require('nw.gui'),
+
+    // browser window object
+    win = gui.Window.get(),
+
+    // os object
+    os = require('os'),
+
+    // path object
+    path = require('path'),
+
+    // fs object
+    fs = require('fs'),
+
+    // TMP Folder
+    tmpFolder = path.join(os.tmpDir(), 'TTY-Logger'),
+
+    moment = require('moment');
+
+/**
+* Windows 8 Fix
+* https://github.com/rogerwang/node-webkit/issues/1021#issuecomment-34358536
+ # commented this line so we can watch movies withou the taskbar showing always
+(process.platform === 'win32' && parseFloat(os.release(), 10) > 6.1) {
+    gui.Window.get().setMaximumSize(screen.availWidth + 15, screen.availHeight + 14);
+};
+
+*/
+// Create the System Temp Folder. This is used to store temporary data like movie files.
+if( ! fs.existsSync(tmpFolder) ) { fs.mkdir(tmpFolder); }
+
+deleteFolder = function(path) {
+	if( typeof path != 'string' ) return;
+	try {
+		var files = [];
+		if( fs.existsSync(path) ) {
+			files = fs.readdirSync(path);
+			files.forEach(function(file,index){
+				var curPath = path + "/" + file;
+				if(fs.lstatSync(curPath).isDirectory()) {
+					deleteFolder(curPath);
+				} else {
+					fs.unlinkSync(curPath);
+				}
+			});
+			fs.rmdirSync(path);
+		}
+	} catch(err) {
+		console.log('deleteFolder()',err);
+	}
+}
+
+// Wipe the tmpFolder when closing the app (this frees up disk space)
+win.on('close', function(){
+    deleteFolder(tmpFolder);
+    win.close(true);
+});
+
+
+// Developer Shortcuts
+Mousetrap.bind(['shift+f12', 'f12'], function(e) {
+	win.showDevTools();
+}
+ousetrap.bind('f11', function(e) {
+	winreloadIgnoringCache();
+});
+
+// Special Debug Console Calls!
+console.logger = {};
+console.logger.log = console.log.bind(console);
+console.logger.debug = function() {
+	var params = Array.prototype.slice.call(arguments, 1);
+	params.unshift('%c[%cDEBUG%c] ' + arguments[0], 'color: black;', 'color: #00eb76;', 'color: black;');
+	console.debug.apply(console, params);
+}
+console.logger.info = function() {
+	var params = Array.prototype.slice.call(arguments, 1);
+	params.unshift('[%cINFO%c] ' + arguments[0], 'color: blue;', 'color: black;');
+	console.info.apply(console, params);
+}
+console.logger.warn = function() {
+	var params = Array.prototype.slice.call(arguments, 1);
+	params.unshift('[%cWARNING%c] ' + arguments[0], 'color: #ffc000;', 'color: black;');
+	console.warn.apply(console, params);
+}
+console.logger.error = function() {
+	var params = Array.prototype.slice.call(arguments, 1);
+	params.unshift('%c[%cERROR%c] ' + arguments[0], 'color: black;', 'color: #ff1500;', 'color: black;');
+	console.error.apply(console, params);
+}
+
+
+/**
+* Drag n' Drop Torrent Onto PT Window to start playing (ALPHA)
+*/
+window.ondragover = function(e) { e.preventDefault(); return false };
+window.ondrop = function(e) { e.preventDefault(); return false };
+var holder = $('body')[0];
+holder.ondragover = function () { this.classList.add('dragging'); return false; };
+holder.ondragend = function () { this.classList.remove('dragging'); return false; };
+holder.ondrop = function (e) { e.preventDefault(); return false; };
+
+/**
+ * Show 404 page on uncaughtException
+ */
+process.on('uncaughtException', function(err) {
+    window.console.error(err, err.stack);
+});
